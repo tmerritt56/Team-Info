7598b5ca4f5d2316ee1657f4d5173b15ad64b5fe
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-08-21 19:45:01 -0500

    employee test works

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,1 +1,59 @@
-// parent clss: name, id, email, getName(),getId(), getEmail(),getRole() returns employee. Consider adding validation to ensure user input is provided in proper expectations. 
+// parent class: name, id, email, getName(),getId(), getEmail(),getRole() returns employee. Consider adding validation to ensure euser input is provided in proper expectations. 
+const Employee = require("../lib/employee");
+
+describe("Employee", () => {
+    it("creates a new Employee", () => {
+        const emp = new Employee();
+        expect(typeof(emp)).toBe("object");
+    });
+});
+
+    it("can set name", () => {
+        const name = "Tom";
+        const emp = new Employee(name);
+        expect(emp.name).toBe(name);
+    });
+
+    it("Can set ID", () => {
+        const value = 50;
+        const emp = new Employee("Todd", value);
+        expect(emp.id).toBe(value);
+    });
+
+    it("can set email", () => {
+        const email = "test@test.com";
+        const emp = new Employee("Josh", 34, email);
+        expect(emp.email).toBe(email);
+    });
+
+describe("getName", () => {
+    it("get name via getName()", () => {
+        const name = "Tom";
+        const emp = new Employee(name);
+        expect(emp.getName()).toBe(name);
+    });
+});
+
+describe("getId", () => {
+    it("can get id via getID()", () => {
+        const id = 13;
+        const emp = new Employee("Todd", id);
+        expect(emp.getId()).toBe(id);
+    });
+});
+
+describe("getEmail", () => {
+    it("can get email via getEmail()", () => {
+        const email = "test@test.com";
+        const emp = new Employee("Josh", 34, email);
+        expect(emp.getEmail()).toBe(email);
+    });
+});
+describe("getRole", () => {
+    it("can get role via getRole()", () => {
+        const role = "Employee";
+        const emp = new Employee("Todd", 13, "test@test.com");
+        expect(emp.getRole()).toBe(role);
+    });
+});
+
--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,1 +1,14 @@
 // extending employee: github,getGithub(), getRole() overridden to return engineer.
+const Engineer = require("../lib/engineer");
+
+test("get GITHUB User", () => {
+    const User = "GithubUser";
+    const eng = new Engineer ("Todd", 1, "test@test.com", User);
+    expect(eng.github).toBe(User);
+});
+
+test("getRole() should return Eng", () => {
+    const role = "Engineer";
+    const eng = new Engineer ("Todd", 1, "test@test.com", "GitHubUser");
+    expect(eng.getRole()).toBe(role)
+})
